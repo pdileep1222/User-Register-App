f17e767a29eed71379437462a5ef3d5b7965fb13
pdileep1222 <pdileep1222@gmail.com>
2025-04-10 12:25:17 +0530

    Update DBInfo.java

--- User-Reg61-Project/src/main/java/test/DBInfo.java
+++ User-Reg61-Project/src/main/java/test/DBInfo.java
@@ -4,4 +4,5 @@ public interface DBInfo
    public static final String dbUrl="jdbc:oracle:thin:@localhost:1521:orcl";
    public static final String uName="mydb1";
    public static final String pWord="123";
+   
 }
